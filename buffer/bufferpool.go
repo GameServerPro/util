3c4a024f39e7975d7b090bbc0e6fe61caa5d6466
zerak <Kingbug2010@126.com>
2016-09-01 18:49:43 +0800

    bp code format

--- buffer/bufferpool.go
+++ buffer/bufferpool.go
@@ -14,21 +14,6 @@ type BufferPool struct {
 	collection chan *Buffer
 }
 
-func NewBufferPool(initSize, maxSize int) *BufferPool {
-	if maxSize < initSize {
-		maxSize = initSize
-	}
-	bp := &BufferPool{
-		pool:       &sync.Pool{New: func() interface{} { return NewBuffer() }},
-		collection: make(chan *Buffer, maxSize),
-	}
-	for i := 0; i < initSize; i++ {
-		bp.pool.Put(bp.pool.New())
-	}
-	go bp.run()
-	return bp
-}
-
 func (bp *BufferPool) Get() (buf *Buffer, fromPool bool) {
 	bufObj := bp.pool.Get()
 	if bufObj == nil {
@@ -59,4 +44,19 @@ func (bp *BufferPool) run() {
 			bp.pool.Put(buf)
 		}
 	}
+}
+
+func NewBufferPool(initSize, maxSize int) *BufferPool {
+	if maxSize < initSize {
+		maxSize = initSize
+	}
+	bp := &BufferPool{
+		pool:       &sync.Pool{New: func() interface{} { return NewBuffer() }},
+		collection: make(chan *Buffer, maxSize),
+	}
+	for i := 0; i < initSize; i++ {
+		bp.pool.Put(bp.pool.New())
+	}
+	go bp.run()
+	return bp
 }
